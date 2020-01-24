db7332ac650e928f73e10e7fc832aa4c7022cf6c
sanaz <sanazmarand@gmail.com>
2020-01-24 14:50:38 -0600

    added changes

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    class Car
+    {
+        public int Year;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string engineNoise) 
+        {
+            EngineNoise = engineNoise;
+        }
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            HonkNoise = honkNoise;
+        }
+         
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,6 +6,7 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var carLot = new CarLot();
             //TODO
 
             //Create a seperate class file called Car
@@ -16,8 +17,42 @@ namespace CarLotSimulator
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
+            var newCar = new Car();
+            newCar.EngineNoise = "Vroom";
+            newCar.Make = "Ford";
+            newCar.Model = "Escape";
+            newCar.HonkNoise = "Loud";
+            newCar.IsDriveable = true;
+            newCar.Year = 2006;
+
+            
+            
+
+            Console.WriteLine($"{newCar.Year}, {newCar.Make}, {newCar.Model}, {newCar.HonkNoise}, {newCar.EngineNoise}, {newCar.IsDriveable}");
+
+            var sanazsCar = new Car()
+            {
+                Year = 2014,
+                Make = "Ford",
+                Model = "Escape",
+                EngineNoise = "Room Room",
+                HonkNoise = "Beep",
+                IsDriveable = true,
+            };
+            
+                
+
+            var saharsCar = new Car();
+            var momsCar = new Car();
             //Call each of the methods for each car
 
+            carLot.CarCollection.Add(newCar);
+            carLot.CarCollection.Add(momsCar);
+
+            foreach(var car in carLot.CarCollection)
+            {
+                Console.WriteLine($"{car.Model}");
+            }
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
